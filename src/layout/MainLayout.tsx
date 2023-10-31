082c3d06aa6ab729dde6e4f4087be1e572e02821
BenitoJuárez <it@gswift.community>
2023-10-31 19:24:28 +0100

    feat(layout): deleted Header and Footer from layout

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,9 +1,3 @@
-import { Footer, Header } from './components'
-
 export const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <>
-    <Header />
-    <main className="layout-container flex-1">{children}</main>
-    <Footer />
-  </>
+  <main className="layout-container flex-1">{children}</main>
 )
