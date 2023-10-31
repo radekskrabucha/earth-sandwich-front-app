9d1464b188a42480a993b80be98dcf2f3c8b5d2a
radekskrabucha <rskrabucha13@gmail.com>
2023-10-31 21:13:37 +0100

    fix(herosection): formatted HeroSection

--- src/features/home/components/HeroSection.tsx
+++ src/features/home/components/HeroSection.tsx
@@ -9,7 +9,7 @@ export const HeroSection = () => (
     }}
   >
     <section className="layout-section h-screen flex-1 items-center justify-center gap-8">
-      <h2 className="max-xs:text-3xl max-xxs:text-2xl max-xs:p-6 rounded-3xl bg-background px-24 py-20 font-main text-8xl font-bold uppercase text-primary max-xl:px-32 max-xl:py-24 max-xl:text-6xl max-md:p-10 max-md:text-5xl max-sm:text-4xl">
+      <h2 className="rounded-3xl bg-background px-24 py-20 font-main text-8xl font-bold uppercase text-primary max-xl:px-32 max-xl:py-24 max-xl:text-6xl max-md:p-10 max-md:text-5xl max-sm:text-4xl max-xs:p-6 max-xs:text-3xl max-xxs:text-2xl">
         {AppName}
       </h2>
     </section>
