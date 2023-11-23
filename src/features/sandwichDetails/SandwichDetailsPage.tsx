6e42ca17c7f1c5c0b134836da1d3d411af9fd578
radekskrabucha <rskrabucha13@gmail.com>
2023-11-23 14:55:22 +0100

    feat(sandwich-details): added accept and cook buttons to SandwichDetailsPage

--- src/features/sandwichDetails/SandwichDetailsPage.tsx
+++ src/features/sandwichDetails/SandwichDetailsPage.tsx
@@ -1,5 +1,27 @@
-export const SandwichDetailsPage = () => (
-  <section className="layout-section">
-    <h1>Sandwich Details Page</h1>
+import { Button } from '@/components/Button'
+import { Link } from '@/components/Link'
+import { InternalLink } from '@/config/app'
+import type { SandwichDetailsPageProps } from './types'
+
+export const SandwichDetailsPage: React.FC<SandwichDetailsPageProps> = ({
+  params: { id }
+}) => (
+  <section className="layout-section items-center">
+    <div className="flex items-center gap-6">
+      <Button
+        asChild
+        variant="primary"
+      >
+        <Link href={InternalLink.sandwichDetails(id, '/accept-invite')}>
+          Accept
+        </Link>
+      </Button>
+      <Button
+        asChild
+        variant="outline"
+      >
+        <Link href={InternalLink.sandwichDetails(id, '/mint')}>Cook</Link>
+      </Button>
+    </div>
   </section>
 )
