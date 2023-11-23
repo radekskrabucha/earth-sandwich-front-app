9d1e3faeea306e418b61df7f2d71560468d1e6e8
radekskrabucha <rskrabucha13@gmail.com>
2023-11-23 20:04:00 +0100

    feat(sandwich): added sandwich metadata utils

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,7 @@
+import { getUnixTime } from 'date-fns'
+
 export const getCurrentYear = () => new Date().getFullYear()
 
 export const getDateNowISOString = () => new Date().toISOString()
+
+export const getNowUnix = () => getUnixTime(new Date())
