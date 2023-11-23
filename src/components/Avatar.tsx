944a34f8b1f7ed119c43c53c43b65e346d192d6b
radekskrabucha <rskrabucha13@gmail.com>
2023-11-23 14:25:49 +0100

    feat(sandwich-details): added PendingSandwichDetails

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,16 +1,17 @@
 import AvatarPlaceholder from '@/public/images/shared/profile-placeholder.webp'
-import type { Undefinable, WithClassName } from '@/types/common'
+import type { Undefinable } from '@/types/common'
 import type { ImageProps, ImageSrc } from '@/types/image'
 import { cn } from '@/utils/styles'
 import { IconWrapper } from './IconWrapper'
 import { Image } from './Image'
 
-type AvatarProps = {
+export type AvatarProps = {
   avatarProps?: Omit<ImageProps, 'src'>
   src: Undefinable<ImageSrc>
+  className?: string
 }
 
-export const Avatar: React.FC<WithClassName<AvatarProps>> = ({
+export const Avatar: React.FC<AvatarProps> = ({
   className,
   avatarProps,
   src = AvatarPlaceholder
--- src/features/sandwichDetails/components/PendingSandwichDetails.tsx
+++ src/features/sandwichDetails/components/PendingSandwichDetails.tsx
@@ -1,19 +1,81 @@
+import { ProfileInfoWrapper } from '@/components/ProfileInfoWrapper'
 import { RestrictedProfileWrapper } from '@/components/RestrictedProfileWrapper'
 import { InternalLink } from '@/config/app'
 import type { SandwichRaw } from '@/models/sandwich'
+import { SandwichUser } from './SandwichUser'
 
 export const PendingSandwichDetails: React.FC<SandwichRaw> = ({
   owner,
-  name
+  name,
+  participantAddresses
 }) => (
   <RestrictedProfileWrapper
     pushTo={InternalLink.profile(owner, '')}
     shouldBeConnected
   >
-    <section className="layout-section">
-      <h3 className="font-main text-2xl">{name}</h3>
-      <p className="text-center">
+    <section className="layout-section items-center gap-16 text-center">
+      <div className="flex flex-col gap-2">
+        <span className="text-sm text-white/50">Sandwich name</span>
+        <h3 className="font-main text-3xl font-bold text-primary">{name}</h3>
+      </div>
+
+      <ProfileInfoWrapper address={owner}>
+        {profile =>
+          profile ? (
+            <SandwichUser
+              avatar={{
+                src: profile.profileImageUrl,
+                className: 'border-4 border-primary h-48 w-48',
+                avatarProps: {
+                  alt: profile.name
+                }
+              }}
+              name={<span className="text-primary">{profile.name}</span>}
+              label="chef"
+              address={owner}
+            />
+          ) : null
+        }
+      </ProfileInfoWrapper>
+
+      <div className="flex flex-col gap-8">
+        <span className="text-sm text-white/50">Sandwich participants</span>
+        <div className="flex flex-row flex-wrap items-center justify-center gap-8">
+          {participantAddresses.map(address => (
+            <ProfileInfoWrapper
+              key={address}
+              address={address}
+            >
+              {profile =>
+                profile ? (
+                  <SandwichUser
+                    avatar={{
+                      src: profile.profileImageUrl,
+                      className: 'border-4 border-white/30 h-32 w-32',
+                      avatarProps: {
+                        alt: profile.name
+                      }
+                    }}
+                    name={profile.name}
+                    address={address}
+                    label="pending"
+                  />
+                ) : null
+              }
+            </ProfileInfoWrapper>
+          ))}
+        </div>
+      </div>
+
+      <p className="max-w-md text-white/50">
         This sandwich is not finalized yet. You can see the participants below.
+        If you are one of them, you can accept the sandwich invitation by
+        clicking the{' '}
+        <strong className="font-bold text-white">Accept button</strong> below.
+        <br />
+        <br />
+        If you are the owner of this sandwich, you can finalize it by clicking
+        the <strong className="font-bold text-white">Cook button</strong> below.
       </p>
     </section>
   </RestrictedProfileWrapper>
